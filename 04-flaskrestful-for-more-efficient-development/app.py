b1cf354ebdf0ed1ecfb6bfd49771b4758647d1ca
shivanshsinghal107 <shivanshsinghal107@gmail.com>
2020-05-09 16:21:45 +0530

    Learned to authenticate & login with flask JWT.

--- 04-flaskrestful-for-more-efficient-development/app.py
+++ 04-flaskrestful-for-more-efficient-development/app.py
@@ -1,12 +1,19 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = 'mee6'
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity) # /auth
 
 items = [] # empty list, temporary database for storing data for our API
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
         return {'item': item}, 200 if item else 404
