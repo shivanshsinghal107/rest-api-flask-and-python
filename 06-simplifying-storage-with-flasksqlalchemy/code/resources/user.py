6467195417bc96eeb3245efbb5ba8a08e54a4e67
shivanshsinghal107 <shivanshsinghal107@gmail.com>
2020-05-11 22:58:01 +0530

    Updated according to model classes

--- 06-simplifying-storage-with-flasksqlalchemy/code/resources/user.py
+++ 06-simplifying-storage-with-flasksqlalchemy/code/resources/user.py
@@ -1,47 +1,6 @@
 import sqlite3
 from flask_restful import Resource, reqparse
-
-class User:
-    def __init__(self, _id, username, password):
-        self.id = _id
-        self.username = username
-        self.password = password
-
-    @classmethod
-    def find_by_username(cls, username):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM users WHERE username = ?"
-        # we always pass tuple in executing query that's why (username,)
-        result = cursor.execute(query, (username,))
-        # taking the first row out of filtered rows
-        row = result.fetchone()
-        if row:
-            row = cls(*row) # passing row as args which will act as (row[0], row[1], row[2])
-        else:
-            row = None
-
-        connection.close()
-        return row
-
-    @classmethod
-    def find_by_id(cls, _id):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM users WHERE id = ?"
-        # we always pass tuple in executing query that's why (_id,)
-        result = cursor.execute(query, (_id,))
-        # taking the first row out of filtered rows
-        row = result.fetchone()
-        if row:
-            row = cls(*row) # passing row as args which will act as (row[0], row[1], row[2])
-        else:
-            row = None
-
-        connection.close()
-        return row
+from models.user import UserModel
 
 class UserRegister(Resource):
     parser = reqparse.RequestParser()
@@ -60,7 +19,7 @@ class UserRegister(Resource):
         data = UserRegister.parser.parse_args()
 
         # step for ensuring that no two users have same username
-        if User.find_by_username(data['username']):
+        if UserModel.find_by_username(data['username']):
             return {"message": "Username already exists. Choose another."}, 400
 
         connection = sqlite3.connect('data.db')
