a090c597795e7944ebfc93423d2b939accb09a8f
shivanshsinghal107 <shivanshsinghal107@gmail.com>
2020-05-11 16:51:39 +0530

    Initialized the section from end of last section.

---
+++ 06-simplifying-storage-with-flasksqlalchemy/code/resources/user.py
@@ -0,0 +1,75 @@
+import sqlite3
+from flask_restful import Resource, reqparse
+
+class User:
+    def __init__(self, _id, username, password):
+        self.id = _id
+        self.username = username
+        self.password = password
+
+    @classmethod
+    def find_by_username(cls, username):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM users WHERE username = ?"
+        # we always pass tuple in executing query that's why (username,)
+        result = cursor.execute(query, (username,))
+        # taking the first row out of filtered rows
+        row = result.fetchone()
+        if row:
+            row = cls(*row) # passing row as args which will act as (row[0], row[1], row[2])
+        else:
+            row = None
+
+        connection.close()
+        return row
+
+    @classmethod
+    def find_by_id(cls, _id):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM users WHERE id = ?"
+        # we always pass tuple in executing query that's why (_id,)
+        result = cursor.execute(query, (_id,))
+        # taking the first row out of filtered rows
+        row = result.fetchone()
+        if row:
+            row = cls(*row) # passing row as args which will act as (row[0], row[1], row[2])
+        else:
+            row = None
+
+        connection.close()
+        return row
+
+class UserRegister(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('username',
+        type = str,
+        required = True,
+        help = "The field 'username' is empty or is not of string type."
+    )
+    parser.add_argument('password',
+        type = str,
+        required = True,
+        help = "This field 'password' is empty or is not of string type."
+    )
+
+    def post(self):
+        data = UserRegister.parser.parse_args()
+
+        # step for ensuring that no two users have same username
+        if User.find_by_username(data['username']):
+            return {"message": "Username already exists. Choose another."}, 400
+
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "INSERT INTO users VALUES (NULL, ?, ?)"
+        cursor.execute(query, (data['username'], data['password']))
+
+        connection.commit()
+        connection.close()
+
+        return {"message": "User successfully created."}, 201
