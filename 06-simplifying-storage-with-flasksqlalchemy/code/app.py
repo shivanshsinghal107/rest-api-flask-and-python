9238f6bb673f91dcef6ffd3e026a2935a2617a80
shivanshsinghal107 <shivanshsinghal107@gmail.com>
2020-05-11 23:01:13 +0530

    Updated for using SQLAlchemy

--- 06-simplifying-storage-with-flasksqlalchemy/code/app.py
+++ 06-simplifying-storage-with-flasksqlalchemy/code/app.py
@@ -7,6 +7,7 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'mee6'
 api = Api(app)
 
@@ -17,4 +18,6 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
     app.run(port = 5000, debug = True)
