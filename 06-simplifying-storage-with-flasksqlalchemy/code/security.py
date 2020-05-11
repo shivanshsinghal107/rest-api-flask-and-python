4310392be12c263db4cd439b065acd25d468fadd
shivanshsinghal107 <shivanshsinghal107@gmail.com>
2020-05-11 22:59:23 +0530

    Updated according to model classes

--- 06-simplifying-storage-with-flasksqlalchemy/code/security.py
+++ 06-simplifying-storage-with-flasksqlalchemy/code/security.py
@@ -1,13 +1,13 @@
 from werkzeug.security import safe_str_cmp
-from resources.user import User
+from models.user import UserModel
 
 def authenticate(username, password):
     # using sqlite database for retrieving user
-    user = User.find_by_username(username)
+    user = UserModel.find_by_username(username)
     if user and safe_str_cmp(user.password, password):
         return user
 
 def identity(payload):
     user_id = payload['identity']
     # using sqlite database for retrieving user
-    return User.find_by_id(user_id)
+    return UserModel.find_by_id(user_id)
