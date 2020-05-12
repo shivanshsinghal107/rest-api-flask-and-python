06b1e793489587e961e319f76fffcf330cdfcfd3
shivanshsinghal107 <shivanshsinghal107@gmail.com>
2020-05-12 13:53:18 +0530

    Updated models using SQLAlchemy

--- 06-simplifying-storage-with-flasksqlalchemy/code/models/item.py
+++ 06-simplifying-storage-with-flasksqlalchemy/code/models/item.py
@@ -1,51 +1,31 @@
-import sqlite3
 from db import db
 
 class ItemModel(db.Model):
-    __tablename__ = items
+    __tablename__ = 'items'
 
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(60))
     price = db.Column(db.Float(precision = 2))
 
-    def __init__(self, name, price):
+    store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
+    store = db.relationship('StoreModel')
+
+    def __init__(self, name, price, store_id):
         self.name = name
         self.price = price
+        self.store_id = store_id
 
     def json(self):
         return {'name': self.name, 'price': self.price}
 
     @classmethod
     def find_by_name(cls, name):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
+        return cls.query.filter_by(name = name).first()
 
-        query = "SELECT * FROM items WHERE name = ?"
-        result = cursor.execute(query, (name,))
-        row = result.fetchone()
-        connection.close()
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
 
-        if row:
-            return cls(*row) # using args, since it is same as cls(row[0], row[1])
-
-    # passing self to both methods and removed @classmethod, because ItemModel
-    # itself represents an item, so inserting an item using self makes more sense
-    def insert(self):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "INSERT INTO items VALUES (?, ?)"
-        cursor.execute(query, (self.name, self.price))
-
-        connection.commit()
-        connection.close()
-
-    def update(self):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "UPDATE items SET price = ? WHERE name = ?"
-        cursor.execute(query, (self.price, self.name))
-
-        connection.commit()
-        connection.close()
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
--- 06-simplifying-storage-with-flasksqlalchemy/code/models/user.py
+++ 06-simplifying-storage-with-flasksqlalchemy/code/models/user.py
@@ -1,50 +1,24 @@
-import sqlite3
 from db import db
 
 class UserModel(db.Model):
-    __tablename__ = users
+    __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(60))
     password = db.Column(db.String(60))
 
-    def __init__(self, _id, username, password):
-        self.id = _id
+    def __init__(self, username, password):
         self.username = username
         self.password = password
 
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
     @classmethod
     def find_by_username(cls, username):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM users WHERE username = ?"
-        # we always pass tuple in executing query that's why (username,)
-        result = cursor.execute(query, (username,))
-        # taking the first row out of filtered rows
-        row = result.fetchone()
-        if row:
-            row = cls(*row) # passing row as args which will act as (row[0], row[1], row[2])
-        else:
-            row = None
-
-        connection.close()
-        return row
+        return cls.query.filter_by(username = username).first()
 
     @classmethod
     def find_by_id(cls, _id):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM users WHERE id = ?"
-        # we always pass tuple in executing query that's why (_id,)
-        result = cursor.execute(query, (_id,))
-        # taking the first row out of filtered rows
-        row = result.fetchone()
-        if row:
-            row = cls(*row) # passing row as args which will act as (row[0], row[1], row[2])
-        else:
-            row = None
-
-        connection.close()
-        return row
+        return cls.query.filter_by(id = _id).first()
